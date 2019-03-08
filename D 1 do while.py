c2af4cf16b08197d6fce81a87ea0f40e69e29d84
KillTheReptiles <duvan.diaz.2019@upb.edu.co>
2019-03-07 21:00:07 -0500

    D.) 1

---
+++ D 1 do while.py
@@ -0,0 +1,7 @@
+n1 = 0
+
+while True:
+    print(n1)
+    n1 = n1 + 2
+    if(n1 > 30):
+        break
---
+++ D 1 while.py
@@ -0,0 +1,6 @@
+print("comienzo")
+print()
+n1=0
+while n1 < 30:
+    n1 = n1 + 2
+    print ("its ", n1)
